5ef1f4bc43f14626a359d80e5f2caa4e89e342f3
Diego Barcellos <diegocb81@gmail.com>
2021-05-10 20:07:28 -0300

    Iniciando o Projeto

---
+++ app/src/main/java/com/cursoandroid/projeto_calculadora_de_gorjeta_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cursoandroid.projeto_calculadora_de_gorjeta_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
