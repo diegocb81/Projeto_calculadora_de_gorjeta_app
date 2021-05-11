43d9082349c88e291acde1ed750a7bf20e919f7c
Diego Barcellos <diegocb81@gmail.com>
2021-05-11 18:00:39 -0300

    Ajustando a seekBar

--- app/src/main/java/com/cursoandroid/projeto_calculadora_de_gorjeta_app/MainActivity.java
+++ app/src/main/java/com/cursoandroid/projeto_calculadora_de_gorjeta_app/MainActivity.java
@@ -3,12 +3,49 @@ package com.cursoandroid.projeto_calculadora_de_gorjeta_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textPorcentagem;
+    private TextView textGorjeta;
+    private TextView textTotal;
+    private SeekBar seekBarGorjeta;
+
+    private double porcentagem = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editValor       = findViewById(R.id.editValor);
+        textGorjeta     = findViewById(R.id.textGorjeta);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textTotal       = findViewById(R.id.textTotal);
+        seekBarGorjeta  = findViewById(R.id.seekBarGorjeta);
+
+        //Adicionar listener Seekbar
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                porcentagem = progress;
+                textPorcentagem.setText(Math.round( porcentagem ) + " %");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
